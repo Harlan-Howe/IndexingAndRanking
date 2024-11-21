9f40e2b61efbc927cdc6b42bb61aa6e95f46a52b
Harlan Howe <harlan.howe@kinkaid.org>
2024-11-21 13:46:30 -0600

    Style and comments + condensed update_color_for_rank from two methods to one.

--- WebpageNode.py
+++ WebpageNode.py
@@ -1,8 +1,9 @@
 from typing import List, Optional
 import tkinter as tk
 
-BOX_HALF_SIZE = 15
-PERCENTAGE_RANK_TO_COLOR_MULTIPLIER = 5000
+BOX_HALF_SIZE = 15  # half the width and height of the page boxes when drawn on the screen.
+PERCENTAGE_RANK_TO_COLOR_MULTIPLIER = 5000  # a factor to multiply the percentage factor (e.g., 0.02) by to get a color
+#                                             value, 0-255 that looks good.
 
 
 def rgb_to_color(r: int, g: int, b: int) -> str:
@@ -19,16 +20,18 @@ def rgb_to_color(r: int, g: int, b: int) -> str:
 class WebpageNode:
 
     def __init__(self, id_num: int, page_content: str, xPos: int, yPos: int, links: List[int]):
-        self.id_num = id_num
-        self.content = page_content
-        self.xPos = xPos
-        self.yPos = yPos
-        self.links = links
-        self.color = "#8888ff"
-        self.num_page_visits = 1
-        self.rank = 0.02
-        self.rect_id: Optional[int] = None
-        self.text_id: Optional[int] = None
+        self.id_num: int = id_num
+        self.content: str = page_content
+        self.links: List[int] = links
+
+        # needed for the graphics part of the program.
+        self.xPos:int = xPos
+        self.yPos:int = yPos
+        self.color: str = "#ff0000"  # starting color of this box
+        self.num_page_visits: int = 0
+        self.rank: float = 0.02  # this will be calculated from the num_page_visits.
+        self.rect_id: Optional[int] = None  # tag to identify which box on the screen belongs to this node.
+
 
     def title(self) -> str:
         """
@@ -38,7 +41,6 @@ class WebpageNode:
         end_title_loc = self.content.index("</title>")
         return self.content[7:end_title_loc]
 
-    #  I'm not sure whether you'll need this, but I put it here for completeness, since we have a head() method.
     def body(self) -> str:
         """
         returns just the portion of the web page between the <body> and </body> tags.
@@ -47,26 +49,39 @@ class WebpageNode:
         start_body_loc = self.content.index("<body>")
         return self.content[start_body_loc+6:-7]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.id_num}\t({self.xPos}, {self.yPos})\t{self.title()}->{self.links}"
 
-    def draw_self(self, canvas: tk.Canvas):
-        if self.rect_id is None:
-            self.rect_id = canvas.create_rectangle(self.xPos - BOX_HALF_SIZE, self.yPos - BOX_HALF_SIZE,
-                                                   self.xPos + BOX_HALF_SIZE, self.yPos + BOX_HALF_SIZE,
-                                                   outline="black", fill=self.color, width=2)
-        if self.text_id is None:
-            canvas.create_text(self.xPos-1, self.yPos-1, text=f"{self.id_num}", fill="lightgrey", font="Arial 12 bold")
-            self.text_id = canvas.create_text(self.xPos, self.yPos, text=f"{self.id_num}", fill="black",
-                                              font="Arial 12 bold")
+    def draw_self(self, canvas: tk.Canvas) -> None:
+        """
+        creates the box and text objects that show up on the graphics canvas for this Node. You'll only call this
+        once (per node), and then we can change the created object to alter its appearance elsewhere.
+        :param canvas:  - the drawing area where these shapes wll appear.
+        :return: None
+        """
+        self.rect_id = canvas.create_rectangle(self.xPos - BOX_HALF_SIZE, self.yPos - BOX_HALF_SIZE,
+                                               self.xPos + BOX_HALF_SIZE, self.yPos + BOX_HALF_SIZE,
+                                               outline="black", fill=self.color, width=2)
+        canvas.create_text(self.xPos-1, self.yPos-1, text=f"{self.id_num}", fill="lightgrey", font="Arial 12 bold")
+        canvas.create_text(self.xPos, self.yPos, text=f"{self.id_num}", fill="black", font="Arial 12 bold")
 
-    def recalculate_rank(self, total_steps_taken: int) -> None:
-        self.rank = self.num_page_visits / total_steps_taken
+    def recalculate_rank(self, total_page_visits: int) -> None:
+        """
+        compute the rank for this Node, which will be a number 0.0 - 1.0, calculated from the num_page_visits and the
+        total number of page visits. Ideally, the ranks for all the Nodes will add up to 1.0.
+        :param total_page_visits: how many pages have been visited since the program started? (Ideally the sum of all
+        the num_page_visits for all the nodes will sum up to this.
+        :return: None
+        """
+        self.rank = self.num_page_visits / total_page_visits
 
     def update_color_for_rank(self, canvas: tk.Canvas) -> None:
-        color_val = min(255, max(0, int(self.rank * PERCENTAGE_RANK_TO_COLOR_MULTIPLIER)))
-        self.reset_rect_color(canvas, rgb_to_color(255-color_val, color_val, 0))
+        """
+        calculates and updates the color of the box to reflect the rank of this node from 0.0 -> Red to 1.0 -> Green
+        :param canvas: the drawing area on which this box is drawn.
+        :return:  None
+        """
+        self.color = min(255, max(0, int(self.rank * PERCENTAGE_RANK_TO_COLOR_MULTIPLIER)))
+        canvas.itemconfig(self.rect_id, fill=rgb_to_color(255-self.color, self.color, 0))
 
-    def reset_rect_color(self, canvas: tk.Canvas, color: str) -> None:
-        self.color = color
-        canvas.itemconfig(self.rect_id, fill=color)
+
